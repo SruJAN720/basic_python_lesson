6254a69cefbd7329c29dd3b407c73c935722889a
SruJAN720 <135273955+SruJAN720@users.noreply.github.com>
2025-06-08 17:20:17 -0700

    Add files via upload

---
+++ day2/EP-08.py
@@ -0,0 +1,23 @@
+# # Example of a for loop counting from 1 to 5
+# for i in range(1, 16):
+#     print(i)
+
+# # Example of a while loop counting from 1 to 5
+# counter = 1
+# while counter < 5:
+#     print(counter)
+#     counter += 1
+
+
+# # Example of a pattern using for loop
+# for i in range(1, 6):
+#     print("*" *i)
+
+
+# # Example of a pattern using while loop
+rows = 5
+i = 1
+while i <= rows:
+    print("*" * i)
+    i += 1
+
