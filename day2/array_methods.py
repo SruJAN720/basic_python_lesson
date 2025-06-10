c093b961cf089b133daf9727eaaad79c037ba127
SruJAN720 <135273955+SruJAN720@users.noreply.github.com>
2025-06-09 23:23:48 -0700

    Update array_methods.py

--- day2/array_methods.py
+++ day2/array_methods.py
@@ -1,8 +1,10 @@
-colors = ["red", "blue", "green"]
+colors = ["red", "orange", "yellow", "green", "blue"]
 
-colors.append("yellow")     # Add new color
+colors.append("indigo")     # Add new color
+colors.append("violet")     # Add new color
 colors.remove("blue")       # Remove one color
 last_color = colors.pop()   # Remove the last item and store it
+colors.pop(2)               # Remove the item located at position 2
 position = colors.index("red")  # Find where "red" is
 
 print(colors)
