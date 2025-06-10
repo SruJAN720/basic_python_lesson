a976cbef050ef6d17dedcabc9629041b083425d2
SruJAN720 <135273955+SruJAN720@users.noreply.github.com>
2025-06-09 22:54:54 -0700

    Create introduce_yourself.py

---
+++ day2/introduce_yourself.py
@@ -0,0 +1,18 @@
+myName = "Alex"         # String
+myAge = 11              # Integer
+myHeight = 4.9          # Float (height in feet)
+isMale = True           # Boolean
+
+# Now let's print the info sheet!
+
+print("Your Name:")
+print(myName)
+
+print("Your Age    :")
+print(myAge)
+
+print("Your Height :")
+print(myHeight)
+
+print("Are you Male?")
+print(isMale)
